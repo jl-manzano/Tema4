7922a1d0787eaf602f4cda8b1ace0ce6a587e280
jl-manzano <jl.manzano@iesnervion.es>
2025-03-04 14:37:43 +0100

    por terminar

--- Tema4/src/parte4/ej2/GESTISIMAL.java
+++ Tema4/src/parte4/ej2/GESTISIMAL.java
@@ -2,16 +2,33 @@ package parte4.ej2;
 
 import java.util.HashSet;
 
+import parte4.ej1.Alumno;
+
 public class GESTISIMAL {
-	
+
 	// Creamos el conjunto articulos para almacenar todos los articulos
 	private static HashSet<Articulo> articulos = new HashSet<Articulo>();
-	
+
 	public static void listado() {
-		
-		for (Articulo a : articulos) {
-			System.out.println(a);
-			System.out.println();
+
+		if (articulos.isEmpty()) {
+			System.out.println("No hay artículos en el almacén");
+		} else {
+			for (Articulo a : articulos) {
+				System.out.println(a);
+			}
 		}
+
 	}
+	
+	public static void alta(Articulo articulo) {
+		articulos.add(articulo);
+		System.out.println("Artículo añadido con éxito");
+	}
+
+//	public static void baja(String nombre) {
+//		articulos.remove(articulo);
+//		System.out.println("Artículo añadido con éxito");
+//	}
+	
 }
